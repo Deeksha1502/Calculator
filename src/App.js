9b4b1dbb9c1084a2792e1bbdb8202072faf2e98d
Deeksha <deekshad1502@gmail.com>
2024-07-02 15:41:58 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-eval */
+import { evaluate } from 'mathjs';
 import { useState } from 'react';
 import './App.css';
 
@@ -28,30 +29,58 @@ export const App = () => {
 
   const [value, setValue] = useState('');
 
-  const handleChange = (e) => {};
+  const handleClick = (item) => {
+    if (item === 'AC') {
+      setValue('');
+    } else if (item === 'DEL') {
+      setValue(value.slice(0, -1));
+    } else if (item === '=') {
+      try {
+        setValue(evaluate(value).toString());
+      } catch (error) {
+        setValue('Error');
+      }
+    } else {
+      const operators = ['%', '/', '*', '-', '+'];
+      const lastChar = value.slice(-1);
 
-  const handleClick = (e) => {};
+      if (
+        (operators.includes(item) && operators.includes(lastChar)) ||
+        (item === '.' && value.includes('.'))
+      ) {
+        return;
+      }
 
-  const handleSubmit = (e) => {};
+      setValue((prevValue) => prevValue + item);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='App'>
       <div>
-        <h1>Calculator</h1>
+        <h1 className='heading'>Calculator</h1>
       </div>
       <div className='outer-container'>
         <form onSubmit={handleSubmit}>
-          <input type='text' className='input-box' onChange={handleChange} value={value}></input>
+          <input
+            type='text'
+            className='input-box'
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </form>
-        <div className='inner-container' onClick={handleClick}>
-          {arr.map((item, index) => {
-            return (
-              <div key={index}>
-                <button className='cells' id={index}>
-                  {item}
-                </button>
-              </div>
-            );
-          })}
+        <div className='inner-container'>
+          {arr.map((item, index) => (
+            <div key={index}>
+              <button className='cells' id={index} onClick={() => handleClick(item)}>
+                {item}
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
